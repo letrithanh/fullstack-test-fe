a9a57f2d7c3d8cff94396590e48a2dcb12fd5f29
Thanh Le <letrithanh@outlook.com>
2025-04-05 12:44:52 +0700

    feat(event-list) - remove unused code

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,7 +1,5 @@
 import EventsServerService from "@/control/events/events-service.server";
 
-export const dynamic = "force-static";
-
 export async function GET() {
     const eventsServerService = new EventsServerService();
     const events = await eventsServerService.getEvents();

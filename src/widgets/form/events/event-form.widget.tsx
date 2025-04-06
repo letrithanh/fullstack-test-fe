c5ebd6aefa37ac555d8a9b8212e64aadcad054f2
Thanh Le <letrithanh@outlook.com>
2025-04-06 14:12:20 +0700

    feat(update-event) - update an existed event by selected event

--- src/widgets/form/events/event-form.widget.tsx
+++ src/widgets/form/events/event-form.widget.tsx
@@ -1,80 +1,100 @@
-"use client"
+"use client";
 
 import Input from "../../../components/form/input/input.component";
 import TextArea from "../../../components/form/textarea/textarea.component";
 import DatePicker from "../../../components/form/datepicker/date-picker.component";
 import InputValidation from "@/hooks/input-validation.interface";
 import { useInput } from "@/hooks/use-input.hook";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { HOME_PATH } from "@/utils/application-path";
 import { EventsClientService } from "@/control/events/events-service.client";
+import { useApplication } from "@/hooks/client/application/application.hook";
 
 export default function EventFormWidget() {
-
     const router = useRouter();
 
-    const [assignedTitle, titleAction] = useInput("")
-    const [assignedDescription, descriptionAction] = useInput("")
-    const [date, setDate] = useState(new Date())
-    const [assignedLocation, locationAction] = useInput("")
-    const [assignedMaxAttendee, maxAttendeeAction] = useInput("")
+    const [assignedTitle, titleAction] = useInput("");
+    const [assignedDescription, descriptionAction] = useInput("");
+    const [date, setDate] = useState(new Date());
+    const [assignedLocation, locationAction] = useInput("");
+    const [assignedMaxAttendee, maxAttendeeAction] = useInput("");
     const [isSubmit, setSubmit] = useState(false);
 
+    const {
+        selectedEvent: [assignedSelectedEvent, selectedEventAction],
+    } = useApplication();
+
+    useEffect(() => {
+        if (isEditMode()) {
+            titleAction.update(`${assignedSelectedEvent.event?.title}`);
+            descriptionAction.update(`${assignedSelectedEvent.event?.description}`);
+            setDate(new Date(`${assignedSelectedEvent.event?.date}`));
+            locationAction.update(`${assignedSelectedEvent.event?.location}`);
+            maxAttendeeAction.update(`${assignedSelectedEvent.event?.maxAttendees}`);
+        }
+    }, [])
+
+    function isEditMode() {
+        return selectedEventAction.hasValue();
+    }
+
     function titleValidation(): InputValidation {
-        const isTitleBlank = assignedTitle.value.trim().length == 0
+        const isTitleBlank = assignedTitle.value.trim().length == 0;
         if ((titleAction.isValueChanged() || isSubmit) && isTitleBlank) {
             return {
                 isError: true,
-                errorMessage: <>Title must have value</>
-            }
+                errorMessage: <>Title must have value</>,
+            };
         }
 
-        const isExceedMaxLength = assignedTitle.value.length > 100
+        const isExceedMaxLength = assignedTitle.value.length > 100;
         if (isExceedMaxLength) {
             return {
                 isError: true,
-                errorMessage: <>Max length is 100 characters</>
-            }
+                errorMessage: <>Max length is 100 characters</>,
+            };
         }
 
         return {
-            isError: false
-        }
+            isError: false,
+        };
     }
 
     function descriptionValidation(): InputValidation {
-        const isDescriptionBlank = assignedDescription.value.trim().length == 0
-        if ((descriptionAction.isValueChanged() || isSubmit) && isDescriptionBlank) {
+        const isDescriptionBlank = assignedDescription.value.trim().length == 0;
+        if (
+            (descriptionAction.isValueChanged() || isSubmit) &&
+            isDescriptionBlank
+        ) {
             return {
                 isError: true,
-                errorMessage: <>Description must have value</>
-            }
+                errorMessage: <>Description must have value</>,
+            };
         }
 
-        const isExceedMaxLength = assignedDescription.value.length > 500
+        const isExceedMaxLength = assignedDescription.value.length > 500;
         if (isExceedMaxLength) {
             return {
                 isError: true,
-                errorMessage: <>Max length is 500 characters</>
-            }
+                errorMessage: <>Max length is 500 characters</>,
+            };
         }
 
         return {
-            isError: false
-        }
+            isError: false,
+        };
     }
 
     function isDateValid(checkedDate: Date): boolean {
         const today = new Date();
         today.setHours(0, 0, 0, 0);
 
-        return checkedDate >= today
+        return checkedDate >= today;
     }
 
     function onSelectDate(selectedDate: Date) {
-        if(isDateValid(selectedDate)) {
-            
+        if (isDateValid(selectedDate)) {
             setDate(selectedDate);
         } else {
             setDate(new Date());
@@ -82,77 +102,118 @@ export default function EventFormWidget() {
     }
 
     function locationValidation(): InputValidation {
-        const isLocationBlank = assignedLocation.value.trim().length == 0
+        const isLocationBlank = assignedLocation.value.trim().length == 0;
         if ((locationAction.isValueChanged() || isSubmit) && isLocationBlank) {
             return {
                 isError: true,
-                errorMessage: <>Location must have value</>
-            }
+                errorMessage: <>Location must have value</>,
+            };
         }
 
-        const isExceedMaxLength = assignedLocation.value.length > 200
+        const isExceedMaxLength = assignedLocation.value.length > 200;
         if (isExceedMaxLength) {
             return {
                 isError: true,
-                errorMessage: <>Max length is 200 characters</>
-            }
+                errorMessage: <>Max length is 200 characters</>,
+            };
         }
 
         return {
-            isError: false
-        }
+            isError: false,
+        };
     }
 
     function maxAttendeeValidation(): InputValidation {
-        const isMaxAttendeeBlank = assignedMaxAttendee.value.trim().length == 0
-        if ((maxAttendeeAction.isValueChanged() || isSubmit) && isMaxAttendeeBlank) {
+        const isMaxAttendeeBlank = assignedMaxAttendee.value.trim().length == 0;
+        if (
+            (maxAttendeeAction.isValueChanged() || isSubmit) &&
+            isMaxAttendeeBlank
+        ) {
             return {
                 isError: true,
-                errorMessage: <>Max attendee must have value</>
-            }
+                errorMessage: <>Max attendee must have value</>,
+            };
         }
 
-        const inRange = parseInt(assignedMaxAttendee.value) > 0 && parseInt(assignedMaxAttendee.value) <= 100
+        const inRange =
+            parseInt(assignedMaxAttendee.value) > 0 &&
+            parseInt(assignedMaxAttendee.value) <= 100;
         if (maxAttendeeAction.isValueChanged() && !inRange) {
             return {
                 isError: true,
-                errorMessage: <>Max attendee must greater than 0, and less than or equals to 100</>
-            }
+                errorMessage: (
+                    <>
+                        Max attendee must greater than 0, and less than or
+                        equals to 100
+                    </>
+                ),
+            };
         }
 
         return {
-            isError: false
-        }
+            isError: false,
+        };
+    }
+
+    function onExit() {
+        selectedEventAction.reset();
     }
 
     function onCancel() {
-        router.push(HOME_PATH)
+        onExit();
+        router.push(HOME_PATH);
     }
 
     function onSave(event: React.FormEvent<HTMLButtonElement>) {
         event.preventDefault();
         setSubmit(true);
 
-        const isValidData = titleAction.isValueChanged() && !titleValidation().isError 
-                            && descriptionAction.isValueChanged() && !descriptionValidation().isError
-                            && isDateValid(date)
-                            && locationAction.isValueChanged() && !locationValidation().isError
-                            && maxAttendeeAction.isValueChanged() && !maxAttendeeValidation().isError;
-        
+        const isValidData =
+            titleAction.isValueChanged() &&
+            !titleValidation().isError &&
+            descriptionAction.isValueChanged() &&
+            !descriptionValidation().isError &&
+            isDateValid(date) &&
+            locationAction.isValueChanged() &&
+            !locationValidation().isError &&
+            maxAttendeeAction.isValueChanged() &&
+            !maxAttendeeValidation().isError;
+
         if (isValidData) {
             const eventsClientService = new EventsClientService();
-            eventsClientService.createEvent({
+            const data = {
+                id: assignedSelectedEvent.event?.id,
                 title: assignedTitle.value,
                 description: assignedDescription.value,
                 location: assignedLocation.value,
                 date: date.toISOString(),
-                maxAttendees: parseInt(assignedMaxAttendee.value)
-            }).then(() => {
-                router.push(HOME_PATH);
-            }).catch(onerror => {
-                console.error(onerror);
-                // display popup error
-            })
+                maxAttendees: parseInt(assignedMaxAttendee.value),
+            }
+
+            if (isEditMode()) {
+                console.log("isEditMode")
+                eventsClientService
+                    .updateEvent(data)
+                    .then(() => {
+                        onExit();
+                        router.push(HOME_PATH);
+                    })
+                    .catch((onerror) => {
+                        console.error(onerror);
+                        // display popup error
+                    });
+            } else {
+                eventsClientService
+                    .createEvent(data)
+                    .then(() => {
+                        onExit();
+                        router.push(HOME_PATH);
+                    })
+                    .catch((onerror) => {
+                        console.error(onerror);
+                        // display popup error
+                    });
+            }
         }
     }
 
@@ -173,10 +234,10 @@ export default function EventFormWidget() {
                     <div className="px-4 py-6 sm:p-8">
                         <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="col-span-full">
-                                <Input 
-                                    label="Title" 
-                                    rightLabel="required" 
-                                    placeholder="Event's name" 
+                                <Input
+                                    label="Title"
+                                    rightLabel="required"
+                                    placeholder="Event's name"
                                     {...assignedTitle}
                                     {...titleValidation()}
                                 />
@@ -200,14 +261,16 @@ export default function EventFormWidget() {
                                     onSelectDate={onSelectDate}
                                     date={date}
                                     key={date.toISOString()}
+                                    isError={!isDateValid(date)}
+                                    errorMessage={isDateValid(date) ? undefined : "Date must greater than today"}
                                 />
                             </div>
 
                             <div className="col-span-full">
-                                <Input 
-                                    label="Location" 
-                                    rightLabel="required" 
-                                    placeholder="Event's location" 
+                                <Input
+                                    label="Location"
+                                    rightLabel="required"
+                                    placeholder="Event's location"
                                     {...assignedLocation}
                                     {...locationValidation()}
                                 />
